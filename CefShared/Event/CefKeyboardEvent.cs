549fd7e0356077779c3d1e042ed25a07e5307185
Riekelt Brands <riekeltbrands291@gmail.com>
2021-03-30 19:23:44 +0200

    Whoops, broken

---
+++ CefShared/Event/CefKeyboardEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Lidgren.Network;
+
+namespace CefShared.Event
+{
+    public class CefKeyboardEvent : CefEvent
+    {
+        public string InputString;
+
+        public override int GetEventID()
+        {
+            return 2000;
+        }
+
+        public override NetOutgoingMessage Serialize(NetOutgoingMessage message)
+        {
+            message.Write(InstanceID);
+            message.Write(InputString);
+
+            return message;
+        }
+
+        public override void Deserialize(NetIncomingMessage message)
+        {
+            InstanceID = message.ReadString();
+            InputString = message.ReadString();
+        }
+    }
+}
---
+++ CefShared/Event/CefMouseEvent.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Lidgren.Network;
+
+namespace CefShared.Event
+{
+    public class CefMouseEvent : CefEvent
+    {
+        public int MouseButton;
+
+        public bool MouseButtonDown;
+
+        public int MouseX;
+
+        public int MouseY;
+
+        public override int GetEventID()
+        {
+            return 2001;
+        }
+
+        public override NetOutgoingMessage Serialize(NetOutgoingMessage message)
+        {
+            message.Write(InstanceID);
+            message.Write(MouseButton);
+            message.Write(MouseButtonDown);
+            message.Write(MouseX);
+            message.Write(MouseY);
+
+            return message;
+        }
+
+        public override void Deserialize(NetIncomingMessage message)
+        {
+            InstanceID = message.ReadString();
+            MouseButton = message.ReadInt32();
+            MouseButtonDown = message.ReadBoolean();
+            MouseX = message.ReadInt32();
+            MouseY = message.ReadInt32();
+        }
+    }
+}
